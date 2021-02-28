25af613932c1b474d07c2600c7a7611e790b5bb3
IngvarListard <vlzvoice@ya.ru>
2021-03-01 00:53:21 +0300

    feature: control bar, context menu realization start

--- pyqee/ui/main_window.py
+++ pyqee/ui/main_window.py
@@ -2,7 +2,8 @@ import os
 
 import mpv
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QMainWindow, QWidget, QToolBar
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import QMainWindow, QWidget, QToolBar, QWidgetAction
 
 from pyqee.ui.resources import qrc_resources
 
@@ -14,14 +15,19 @@ RESPECT_USER_CONFIG = bool(os.environ.get('RESPECT_USER_CONFIG'))  # TODO change
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self._create_menu_bar()
-        self._create_toolbars()
         self.setWindowTitle("Pyqee")
         self.resize(600, 400)
         self.container = QWidget(self)
         self.setCentralWidget(self.container)
+
         self.container.setAttribute(Qt.WA_DontCreateNativeAncestors)
         self.container.setAttribute(Qt.WA_NativeWindow)
+
+        self._create_actions()
+        self._create_menu_bar()
+        self._create_toolbars()
+        # self._create_context_menu()
+
         player = mpv.MPV(
             wid=str(int(self.container.winId())),
             vo='x11',
@@ -41,11 +47,45 @@ class MainWindow(QMainWindow):
         menu_bar = self.menuBar()
 
         file_menu = menu_bar.addMenu('&File')
+        file_menu.addAction(self.open_action)
+
         video_menu = menu_bar.addMenu('&Video')
+        video_menu.addAction(self.play_action)
+        video_menu.addAction(self.pause_action)
+        video_menu.addAction(self.next_track_action)
+        video_menu.addAction(self.prev_track_action)
+
         audio_menu = menu_bar.addMenu('&Audio')
         subtitles_menu = menu_bar.addMenu('&Subtitles')
         help_menu = menu_bar.addMenu('&Help')
 
     def _create_toolbars(self):
         control_bar = QToolBar('Control')
+        control_bar.addActions((
+            self.play_action,
+            self.pause_action,
+            self.next_track_action,
+            self.prev_track_action,
+        ))
         help_toolbar = self.addToolBar(Qt.BottomToolBarArea, control_bar)
+
+    def _create_actions(self):
+        self.open_action = QAction(QIcon(':arrow_down.svg'), '&Open...', self)
+        self.play_action = QAction(QIcon(':arrow_right.svg'), '&Play', self)
+        self.pause_action = QAction(QIcon(':arrow_down.svg'), '&Pause', self)
+        self.stop_action = QAction(QIcon(':arrow_down.svg'), '&Stop', self)
+        self.next_track_action = QAction(QIcon(':arrow_right.svg'), '&Next track', self)
+        self.prev_track_action = QAction(QIcon(':arrow_left.svg'), '&Prev track', self)
+
+    # def _create_context_menu(self):
+    #     self.centralWidget().setContextMenuPolicy(Qt.ActionsContextMenu)
+    #
+    #     self.centralWidget().addAction(self.open_action)
+
+    def mousePressEvent(self, QMouseEvent):
+        print('test')
+        if QMouseEvent.button() == Qt.LeftButton:
+            print("Left Button Clicked")
+        elif QMouseEvent.button() == Qt.RightButton:
+            #do what you want here
+            print("Right Button Clicked")
