a5422f3d2f71a4613c0f4b78ee20ed3927782d8f
IngvarListard <vlzvoice@ya.ru>
2021-03-03 23:04:29 +0300

    feature: context menu solution found

--- pyqee/ui/main_window.py
+++ pyqee/ui/main_window.py
@@ -1,9 +1,10 @@
 import os
 
 import mpv
+from PySide6 import QtCore, QtGui
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction, QIcon
-from PySide6.QtWidgets import QMainWindow, QWidget, QToolBar, QWidgetAction
+from PySide6.QtWidgets import QMainWindow, QWidget, QToolBar, QWidgetAction, QVBoxLayout, QMenu
 
 from pyqee.ui.resources import qrc_resources
 
@@ -18,6 +19,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Pyqee")
         self.resize(600, 400)
         self.container = QWidget(self)
+
         self.setCentralWidget(self.container)
 
         self.container.setAttribute(Qt.WA_DontCreateNativeAncestors)
@@ -26,21 +28,31 @@ class MainWindow(QMainWindow):
         self._create_actions()
         self._create_menu_bar()
         self._create_toolbars()
-        # self._create_context_menu()
+        self._create_context_menu()
+
 
         player = mpv.MPV(
             wid=str(int(self.container.winId())),
-            vo='x11',
+            # vo='x11',
             log_handler=print,
-            loglevel='debug',
-            input_default_bindings=True,
-            input_vo_keyboard=True,
-            player_operation_mode='pseudo-gui',
-            script_opts='osc-layout=box,osc-seekbarstyle=bar,osc-deadzonesize=0,osc-minmousemove=3',
+            # loglevel='debug',
+            # input_default_bindings=True,
+            # input_vo_keyboard=True,
+            # player_operation_mode='pseudo-gui',
+            # script_opts='osc-layout=box,osc-seekbarstyle=bar,osc-deadzonesize=0,osc-minmousemove=3',
             osc=True,
-            config='yes' if RESPECT_USER_CONFIG else 'no',
+            # config='yes' if RESPECT_USER_CONFIG else 'no',
             config_dir=CONFIG_DIR
         )
+
+        def test_(*args):
+            if args[0] == 'dm':
+                self.menu_.exec_(self.container.cursor().pos())
+
+        # player.register_event_callback()
+        player.register_key_binding('MBTN_LEFT', 'cycle fullscreen')
+        player.register_key_binding('MBTN_RIGHT', test_)
+        # player.event_callback()
         player.play('test.webm')
 
     def _create_menu_bar(self):
@@ -76,6 +88,11 @@ class MainWindow(QMainWindow):
         self.stop_action = QAction(QIcon(':arrow_down.svg'), '&Stop', self)
         self.next_track_action = QAction(QIcon(':arrow_right.svg'), '&Next track', self)
         self.prev_track_action = QAction(QIcon(':arrow_left.svg'), '&Prev track', self)
+
+    def _create_context_menu(self):
+        self.menu_ = QMenu()
+        a = self.menu_.addAction("Test1")
+        b = self.menu_.addAction("Test2")
 
     # def _create_context_menu(self):
     #     self.centralWidget().setContextMenuPolicy(Qt.ActionsContextMenu)
